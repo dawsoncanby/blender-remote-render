82201510b90cec5f28c7dfb7f0e779c48475478a
dawsoncanby <dawsoncanby@gmail.com>
2020-08-20 16:50:07 -0600

    update gitignore

--- blender-ui/src/components/RemoteRender.vue
+++ blender-ui/src/components/RemoteRender.vue
@@ -4,9 +4,6 @@
       <div class="title-bar">
         <div class="title-bar-text">Render Blender Projects</div>
         <div class="title-bar-controls">
-          <!-- <button aria-label="Minimize"></button>
-          <button aria-label="Maximize"></button>
-          <button aria-label="Close"></button> -->
         </div>
       </div>
       <div class="window-body">
